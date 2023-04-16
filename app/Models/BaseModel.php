5e774a9731517e42a0299a8a74d77582d9cbf2d0
edysupardi <edy.supardi18@gmail.com>
2023-04-16 16:38:33 +0700

    feat: accessor and mutator model for id primary

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -5,18 +5,62 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\Uuids;
 use App\Traits\PrintLog;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\Crypt;
 
 class BaseModel extends Model
 {
     // helper
     use PrintLog;
 
-    protected $primaryKey = 'id';
-    protected $keyType = 'string';
-    public $incrementing = false;
-
     protected $casts = [
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
     ];
+
+    protected $hidden = ['deleted_at', 'deleted_by'];
+
+    /**
+     *    A C C E S S O R  A N D  M U T A T O R
+     */
+
+    protected function id(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => Crypt::encrypt($value),
+            // set: fn (string $value) => Crypt::decrypt($value),
+        );
+    }
+
+    /**
+     *    S C O P E
+     */
+
+    public function scopeById($q, $id = '')
+    {
+        return $q->where('id', $id);
+    }
+
+    public function scopeByIdEncrypt($id = '')
+    {
+        return $this->scopeById(!empty($id) ? Crypt::decrypt($id) : '');
+    }
+
+    public function scopeByIds($q, $id = [])
+    {
+        return $q->whereIn('id', $id);
+    }
+
+    public function scopeByIdsEncrypt($q, $ids = [])
+    {
+        if(count($ids) > 0){
+            $decp = [];
+            foreach ($ids as $v ) {
+                $decp[] = Crypt::decrypt($v);
+            }
+            return $this->scopeByIds($decp);
+        }
+        return $this->scopeByIds($ids);
+    }
 }
