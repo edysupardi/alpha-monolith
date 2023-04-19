3072a9fe88d912a91bf79aed4ff8cb91e338498f
edysupardi <edy.supardi18@gmail.com>
2023-04-19 22:19:38 +0700

    chore: add latlng branch

---
+++ database/migrations/2023_04_19_151223_add_latlng_to_branch.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('branch', function (Blueprint $table) {
+            $table->double('latitude', 15, 8)->nullable()->default(123.4567);
+            $table->double('longitude', 15, 8)->nullable()->default(123.4567);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('branch', function (Blueprint $table) {
+            $table->dropColumn('latitude');
+            $table->dropColumn('longitude');
+        });
+    }
+};
