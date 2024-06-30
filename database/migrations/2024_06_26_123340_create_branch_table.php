c3828e42a41d37d9b93169f604352dfb1d63d140
edysup <edy.supardi18@gmail.com>
2024-06-30 20:22:52 +0700

    chore: token & job migration

--- database/migrations/2024_06_26_123340_create_branch_table.php
+++ database/migrations/2024_06_26_123340_create_branch_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('phone', 30)->nullable()->comment('can be same with phone of PT/CV');
             $table->text('address')->nullable()->comment('can be same with phone of PT/CV');
             $table->boolean('status')->nullable()->default(true)->comment('0:inactive, 1:active');
-            $table->boolean('main_branch')->nullable()->default(false)->comment('0:not main, 1:is main');
+            $table->boolean('main_branch')->nullable()->default(false)->comment('0:not_main, 1:is_main');
             $table->dateTime('created_at')->useCurrent();
             $table->dateTime('updated_at')->useCurrentOnUpdate()->useCurrent();
             $table->softDeletes();
